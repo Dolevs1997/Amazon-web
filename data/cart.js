06cbc98f4b2c53f48cffe632cf10e86a4eea80d9
Dolev <dolevs1890@gmail.com>
2023-10-13 12:30:13 +0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,15 @@
 
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'))|| 
+[{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 const timeoutProduct = {};
 //add item to the cart
 export function addToCart(productId) {
@@ -24,6 +28,7 @@ export function addToCart(productId) {
   }
   displayAddedItem(productId);
   updateCartQuantity();
+  saveToStorage();
 }
 
 // display on the screen item has been added
@@ -59,5 +64,6 @@ export function removeItemFromCart(productId){
     }
   });
   cart = newCart;
- 
+ saveToStorage();
 }
+
